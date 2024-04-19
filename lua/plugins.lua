72fc3e05ca2459f7017b2f9b45251fe2e5eb33c4
jkopa <jarkopaczewski@gmail.com>
2024-04-19 12:44:07 +0100

    initial migration to lazy

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,53 +1,119 @@
-vim.cmd('packadd packer.nvim')
-
-return require('packer').startup(function()
-    use ('wbthomason/packer.nvim')
+require('lazy').setup({
+    'nvim-lua/plenary.nvim',
+    'wbthomason/packer.nvim',
     --use 'rust-lang/rust.vim'
 
-    use {
+    { 'nvim-telescope/telescope.nvim', tag = '0.1.6' },
+    { 'nvim-treesitter/nvim-treesitter', build = ":TSUpdate" },
 
-        'nvim-telescope/telescope.nvim', tag = '0.1.4',
-        -- or                            , branch = '0.1.x',
-        requires = { {'nvim-lua/plenary.nvim'} }
-    }
+    'nvim-treesitter/nvim-treesitter-context',
 
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        run = function()
-            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-            --ts_update()
-        end,}
+    'nvim-treesitter/playground',
+    'theprimeagen/harpoon',
+    'mbbill/undotree',
+    'tpope/vim-fugitive',
 
-    use ('nvim-treesitter/playground')
-    use ('theprimeagen/harpoon')
-    use ('mbbill/undotree')
-    use ('tpope/vim-fugitive')
-    use ("nvim-treesitter/nvim-treesitter-context");
+    'github/copilot.vim',
 
-    use ('github/copilot.vim')
+    'effkay/argonaut.vim',
 
-    use ('effkay/argonaut.vim')
+    'j-hui/fidget.nvim',
 
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v1.x',
-        requires = {
-              -- LSP Support
-              {'neovim/nvim-lspconfig'},
-              {'williamboman/mason.nvim'},
-              {'williamboman/mason-lspconfig.nvim'},
+      -- LSP Support
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
 
-              -- Autocompletion
-              {'hrsh7th/nvim-cmp'},
-              {'hrsh7th/cmp-buffer'},
-              {'hrsh7th/cmp-path'},
-              {'saadparwaiz1/cmp_luasnip'},
-              {'hrsh7th/cmp-nvim-lsp'},
-              {'hrsh7th/cmp-nvim-lua'},
+      -- Autocompletion
+      'hrsh7th/nvim-cmp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-path',
+      'saadparwaiz1/cmp_luasnip',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-nvim-lua',
 
-              -- Snippets
-              {'L3MON4D3/LuaSnip'},
-              {'rafamadriz/friendly-snippets'},
-          }
-  }
-end)
+      -- Snippets
+      'L3MON4D3/LuaSnip',
+      'rafamadriz/friendly-snippets',
+
+    {'neovim/nvim-lspconfig',
+          config = function()
+              local cmp = require('cmp')
+              local cmp_lsp = require("cmp_nvim_lsp")
+              local capabilities = vim.tbl_deep_extend(
+              "force",
+              {},
+              vim.lsp.protocol.make_client_capabilities(),
+              cmp_lsp.default_capabilities())
+
+              require("fidget").setup({})
+              require("mason").setup()
+              require("mason-lspconfig").setup({
+                  ensure_installed = {
+                      "lua_ls",
+                      "rust_analyzer",
+                      "gopls",
+                  },
+                  handlers = {
+                      function(server_name) -- default handler (optional)
+
+                          require("lspconfig")[server_name].setup {
+                              capabilities = capabilities
+                          }
+                      end,
+
+                      ["lua_ls"] = function()
+                          local lspconfig = require("lspconfig")
+                          lspconfig.lua_ls.setup {
+                              capabilities = capabilities,
+                              settings = {
+                                  Lua = {
+                                      runtime = { version = "Lua 5.1" },
+                                      diagnostics = {
+                                          globals = { "vim", "it", "describe", "before_each", "after_each" },
+                                      }
+                                  }
+                              }
+                          }
+                      end,
+                  }
+              })
+
+              local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+              cmp.setup({
+                  snippet = {
+                      expand = function(args)
+                          require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                      end,
+                  },
+                  mapping = cmp.mapping.preset.insert({
+                      ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+                      ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+                      ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                      ["<C-Space>"] = cmp.mapping.complete(),
+                  }),
+                  sources = cmp.config.sources({
+                      { name = 'nvim_lsp' },
+                      { name = 'luasnip' }, -- For luasnip users.
+                  }, {
+                      { name = 'buffer' },
+                  })
+              })
+
+              vim.diagnostic.config({
+                  -- update_in_insert = true,
+                  float = {
+                      focusable = false,
+                      style = "minimal",
+                      border = "rounded",
+                      source = "always",
+                      header = "",
+                      prefix = "",
+                  },
+              })
+          end
+
+
+      }
+})
