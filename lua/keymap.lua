a3180a86e50576ca9e44ea310249b47957ae191c
jkopa <jarkopaczewski@gmail.com>
2024-06-23 16:07:22 +0100

    add some file ignores and move line

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -32,6 +32,7 @@ map('n', '<leader>gs', '<cmd>Git<CR>')
 map('n', '<leader>gc', '<cmd>Git commit<CR>')
 map('n', '<leader>gp', '<cmd>Git push<CR>')
 
-map('n', '<leader>m', '<cmd>make<CR>')
+map('n', '<C-m>', '<cmd>m -2<CR>')
+map('n', '<C-n>', '<cmd>m +1<CR>')
 
 map('n', '<leader>c', ':lua require("compile-mode").compile()<CR>', opts)
